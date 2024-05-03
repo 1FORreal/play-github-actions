f9f80299d41994ad02cacbe4b0615cc2b8a3d437
Cezar <biculescu.cezar@gmail.com>
2024-05-03 14:39:00 +0300

    version 0.0.3

--- src/main/java/com/go4it/playwithgithub/controller/HelloWorldController.java
+++ src/main/java/com/go4it/playwithgithub/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.go4it.playwithgithub.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,10 @@ public class HelloWorldController {
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello world!");
     }
+
+    @GetMapping("/hello-world/{name}")
+    public ResponseEntity<String> helloWorld(@PathVariable String name) {
+        return ResponseEntity.ok("Hello world, " + name + "!");
+    }
+
 }
